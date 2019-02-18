2cf937aeb0c9d60ce5038a7c4669695fbca717d8
Diego Palomo <diegopalomo@gmail.com>
2019-02-18 15:06:25 +0100

    Add gitignore

--- bin/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO BOOKS_PUBLIC.Book (id, title, price, description, nbofpage, illustrations) VALUES (0, 'Functional Programming in Scala', 49.99, 'Functional Programming in Scala is a serious tutorial for programmers looking to learn FP and apply it to the everyday business of coding.', 301, false);
-INSERT INTO BOOKS_PUBLIC.Book (id, title, price, description, nbofpage, illustrations) VALUES (1, 'Big Data Analytics Beyond Hadoop', 55.99, 'Master alternative Big Data technologies that can do what Hadoop can''t: real-time analytics and iterative machine learning', 240, true);
--- target/test-classes/GebConfig.groovy
+++
@@ -1,6 +0,0 @@
-import org.openqa.selenium.firefox.FirefoxDriver
-
-driver = { new FirefoxDriver() }
-
-reportsDir = new File("target/geb-reports")
-reportOnTestFailureOnly = true
